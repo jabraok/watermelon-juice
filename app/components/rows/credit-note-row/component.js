58aa4e14682f3502ca9cb02bcf9f208e6cdc7cbc
Vinh Luu <vinhlt3@gmail.com>
2016-09-07 09:31:46 +0700

    refactoring code

--- app/components/rows/credit-note-row/component.js
+++
@@ -1,8 +0,0 @@
-import Ember from "ember";
-const { notEmpty } = Ember.computed;
-
-export default Ember.Component.extend({
-  classNames: ["row", "stretch"],
-
-  hasQuantity: notEmpty("quantity")
-});
