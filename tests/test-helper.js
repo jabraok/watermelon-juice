7a919e21b28e72c8c6d84651ed2a443e7631ef2a
Vinh Luu <vinhlt3@gmail.com>
2019-02-18 04:05:04 +0700

    Added QUnit.config.testTimeout

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -3,6 +3,8 @@ import config from 'watermelon-juice/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 
+QUnit.config.testTimeout = 6000;
+
 setApplication(Application.create(config.APP));
 
 start();
